8b329874617e0f2d01b73837166b35e2d8317e1d
efeaydin <efekrmaydn@gmail.com>
2024-07-30 21:19:46 +0100

    Created Bike class and extended to Vehicle

--- src/day31_inheritance/vehicleTask/Car.java
+++ src/day31_inheritance/vehicleTask/Car.java
@@ -2,6 +2,9 @@ package day31_inheritance.vehicleTask;
 
 public class Car extends Vehicle {
 
+    // btw car and vehicle, it's single inheritance
+    // one class can only extend one parent class
+
     public Car(String brand, String model, String color, int year, double price) {
         super(brand, model, color, year, price);
     }
