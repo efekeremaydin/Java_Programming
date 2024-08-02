8753f1800bbd9e53990c392c621102f3885f0b16
efeaydin <efekrmaydn@gmail.com>
2024-08-02 01:57:32 +0100

    Created Boat class and extended to Vehicle

--- src/day31_inheritance/vehicleTask/Boat.java
+++ src/day31_inheritance/vehicleTask/Boat.java
@@ -6,7 +6,7 @@ public class Boat extends Vehicle {
     }
 
     public void sail(){
-        System.out.println();
+        System.out.println(getBrand() + " " + getModel() + " is sailing");
     }
 
 }
