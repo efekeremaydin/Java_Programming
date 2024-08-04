8b14d1562ed0a1d1abb2ce5391e73401d4c96ee8
efeaydin <efekrmaydn@gmail.com>
2024-08-04 01:19:16 +0100

    Different overriden methods created

--- src/day31_inheritance/animal_methodOverriding/Eagle.java
+++ src/day31_inheritance/animal_methodOverriding/Eagle.java
@@ -4,6 +4,11 @@ public class Eagle extends Animal{
     public Eagle(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
+
+    @Override
+    public void eat() {
+        System.out.println("Eagle " + getName() + " is eating snake");
+    }
 }
 
 
--- src/day31_inheritance/animal_methodOverriding/Lion.java
+++ src/day31_inheritance/animal_methodOverriding/Lion.java
@@ -2,7 +2,37 @@ package day31_inheritance.animal_methodOverriding;
 
 public class Lion extends Animal {
 
-    public Lion(String name, String breed, char gender, int age, String size, String color) {
+    private boolean isAfricanLion;
+
+    public Lion(String name, String breed, char gender, int age, String size, String color, boolean isAfricanLion) {
         super(name, breed, gender, age, size, color);
+        setAfricanLion(isAfricanLion);
+    }
+
+    public boolean isAfricanLion() {
+        return isAfricanLion;
+    }
+
+    public void setAfricanLion(boolean africanLion) {
+        isAfricanLion = africanLion;
+    }
+
+
+    @Override
+    public void eat() {
+        System.out.println("Lion " + getName() + " is eating deer");
+    }
+
+    public void roar() {
+
+        System.out.println("Lion " + getName() + " is roaring");
+    }
+
+
+    @Override
+    public String toString() {
+        return  super.toString().replace("}", "") + // this will exclude the } at the end of the method
+                ", African Lion='" + isAfricanLion + '\'' +
+                '}';
     }
 }
