0e558025ffb45564b5ddf5bbedd7e1b0433a0119
efeaydin <efekrmaydn@gmail.com>
2024-09-13 02:02:20 +0100

    created day 33 package and CydeoDevStudent.java

---
+++ src/day33_abstraction/CydeoDevStudent.java
@@ -0,0 +1,44 @@
+package day33_abstraction;
+
+public class CydeoDevStudent {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/*
+Warmup tasks:
+	1. Create a sub class of Person (from day32 package) named CydeoDevStudent (final)
+			Extra Variables:
+				id (final), batchName, batchNumber, programmingLanguage(static & final)
+
+			Extra conditions:
+					1. Age has to be at least 18 years old
+					2. batchName can ONLY be set to "Zero to Hero" or "Alumni Dev"
+					3. batchNumber can not be set to zero or negative
+
+			Override the eat() & drink() methods and include the programmingLanguage variable in methods body
+
+			Override the toString Method and include the id, batchName, batchNumber variables
+
+ */
